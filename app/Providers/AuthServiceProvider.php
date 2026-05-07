b7712c0f36e27bf943cf278f3dd4cb533838c143
candid-ops <abdighafar223@gmail.com>
2026-05-07 03:50:39 -0700

    Completely restore app/Providers with fresh Laravel service providers

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,26 +1,19 @@
-<?php
+﻿<?php
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
+    protected \ = [
         //
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
+        \->registerPolicies();
         //
     }
 }
