6be4b5df87f61c4eee53aef29c1d4e3e066b298e
candid-ops <abdighafar223@gmail.com>
2026-05-07 10:54:33 -0700

    fixed even more errors

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,11 +14,11 @@ class RouteServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request \) {
-            return Limit::perMinute(60)->by(\->user()?->id ?: \->ip());
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
-        \->routes(function () {
+        $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
