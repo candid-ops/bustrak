1632acc7d57949edd3ee0f94c869950989e8c328
candid-ops <abdighafar223@gmail.com>
2026-05-07 10:53:31 -0700

    fixed more errors

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    protected \ = [
+    protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
